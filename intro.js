e563ab2ca0d740a7ebf4ae747bd8330c4f230f11
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-26 10:24:13 +0300

    init

---
+++ intro.js
@@ -0,0 +1,12 @@
+console.log('intro file was loaded.');
+console.log('aha');
+
+let mainTitle = document.getElementById('myTitle');
+
+// css #myTitle
+mainTitle.innerText = 'Js Intro Dynamic';
+
+mainTitle.style.fontSize = '70px';
+
+// pakeisti spalva i tomato
+mainTitle.style.color = 'tomato';
---
+++ kintamieji.js
@@ -0,0 +1,6 @@
+console.log('kintamieji.js loaded');
+
+// kintamasis yra 'dezute' (vieta kompiurerio atmintyje su pavadinimu)
+// pirma karta sukuriant(deklaruojant) kitamaji turim
+// pradeti su 'let' arba 'const'
+let suma = 5 + 19;
