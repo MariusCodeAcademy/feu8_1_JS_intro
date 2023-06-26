a225854f5293cfa3eab6109375d94f0f50831db8
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-26 12:00:02 +0300

    nr

--- nr.js
+++ nr.js
@@ -2,7 +2,20 @@
 console.log('number file');
 
 // 1. sukurti 3 skaiciu kintamuosius num1, num2, num3 - priskirti reiksmes
+let num1 = 15;
+let num2 = 25;
+let num3 = 10;
+
 // 2. atsispausdinti kitamuoius
+console.log(num1);
+console.log(num2);
+console.log(num3);
 // 3. sukurti kitamaji rezultatas1 = ir jame
-// 3.1 sudeto num1 ir num2 ir atimti num3
+// 3.1 sudeti num1 ir num2 ir atimti num3
+let rezultatas1 = num1 + num2 - num3;
+console.log('rezultatas1', rezultatas1);
 // 3.2 pirma sudeti num2 ir num3 ir tada padalinti is num1
+let rezultatas2 = (num2 + num3) / num1;
+console.log('rezultatas2', rezultatas2);
+
+// 3.3 atspausdinti ar num1 yra daugiau uz num2 su palyginimo operatorium (> <)
---
+++ prompt.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('prompt file');
+
+// let gavau = prompt('is kur esate?');
+// console.log(gavau);
+
+let num1 = prompt('pirmas skaicius');
+let num2 = prompt('antras skaicius3');
+
+console.log(num1, num2);
