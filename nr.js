aed2ed0d59c2e26b9253cf3f21ff01353dd036fd
Marius CA <mariuskcodeacademy@gmail.com>
2023-06-26 11:48:31 +0300

    nums

---
+++ nr.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('number file');
+
+// 1. sukurti 3 skaiciu kintamuosius num1, num2, num3 - priskirti reiksmes
+// 2. atsispausdinti kitamuoius
+// 3. sukurti kitamaji rezultatas1 = ir jame
+// 3.1 sudeto num1 ir num2 ir atimti num3
+// 3.2 pirma sudeti num2 ir num3 ir tada padalinti is num1
--- string.js
+++ string.js
@@ -2,5 +2,26 @@
 console.log('strings file');
 
 // sukurti 3 kitamuosius username, town, age, isMarried
+let username = 'Bob';
+let town = 'Kaunas';
+let age = 22;
+let isMarried = true;
 
 // atsispausdinti reiksmes su console.log
+console.log(username, town, age, isMarried);
+
+// prilyginti kitamajam sakinys ir atspausdinti.
+// "As esu James from London. Man yra 50 metu. Ar as esu santuokoje - true"
+
+let sakinys =
+  'As esu ' +
+  username +
+  ' from ' +
+  town +
+  '. Man yra ' +
+  age +
+  ' metu. Ar as esu santuokoje - ' +
+  isMarried;
+console.log(sakinys);
+let sakinysBackTic = `As esu ${username} from ${town}. Man yra ${age} metu. Ar as esu santuokoje - ${isMarried}`;
+console.log(sakinysBackTic);
